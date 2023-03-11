25c163fe0b86e1d402d5ae15bc4c63f6ff8342d6
PATHAN SALMAN KHAN <54095769+salmankhan-prs@users.noreply.github.com>
2023-03-11 12:31:33 +0530

    updated not found handler

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const os = require("os");
 require("dotenv").config();
 const pincode = require("./pincode");
 const { db } = require("./db");
@@ -12,6 +13,9 @@ const PORT = process.env.PORT || 4000;
 //connect to db
 db();
 
+app.get("/", (req, res) => {
+  return res.json({ success: true, message: "the server is up and runing  " });
+});
 //get the pincode details by pincode
 app.get("/api/pincode/:pincodeId", async (req, res) => {
   const pincodeId = req.params.pincodeId;
@@ -53,6 +57,11 @@ app.get("/api/pincode/district/:search", async (req, res) => {
       { $unset: ["_id", "__v"] },
     ])
   );
+});
+
+app.use("*", (req, res) => {
+  res.status(404);
+  return res.json({ success: false, error: "Invalid API call", data: {} });
 });
 
 app.listen(PORT, () => console.log(`server listening on ${PORT}`));
