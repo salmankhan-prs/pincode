2fb0964af29c5c4804c2fcc46b94773e4401c728
PATHAN SALMAN KHAN <54095769+salmankhan-prs@users.noreply.github.com>
2023-03-13 10:16:54 +0530

    res updated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const os = require("os");
 require("dotenv").config();
 const pincode = require("./pincode");
 const { db } = require("./db");
+const marked = require("marked");
 
 const app = express();
 
@@ -13,9 +14,124 @@ const PORT = process.env.PORT || 4000;
 //connect to db
 db();
 
+const pincodeAPI = `
+## Pincode API
+
+This API provides information about Indian pincodes. The data is retrieved from a MongoDB database and can be queried based on different parameters.
+
+### Base URL
+
+\`\`\`
+
+https://pincode.cyclic.app/api/
+
+\`\`\`
+
+### Endpoints
+
+### Get pincode details by pincode
+
+Returns the details of a specific pincode.
+
+- URL: **\`/pincode/:pincodeId\`**
+- Method: **\`GET\`**
+- Parameters:
+    - **\`pincodeId\`** (required): The 6-digit pincode.
+- Response:
+    - Status code: **\`200\`**
+    - Body:
+        
+        \`\`\`json
+        
+        {
+          "officename": "Bethapudi B.O",
+          "pincode": "522502",
+          "officeType": "B.O",
+          "Deliverystatus": "Delivery",
+          "divisionname": "Guntur",
+          "regionname": "Vijayawada",
+          "circlename": "Andhra Pradesh",
+          "Taluk": "Mangalagiri Ho",
+          "Districtname": "Guntur",
+          "statename": "ANDHRA PRADESH"
+        }
+        
+        \`\`\`
+        
+
+### Get pincode details by area
+
+Returns the details of pincodes within a specific area.
+
+- URL: **\`/pincode/area/:search\`**
+- Method: **\`GET\`**
+- Parameters:
+    - **\`search\`** (required): The name of the area to search for.
+- Response:
+    - Status code: **\`200\`**
+    - Body:
+        
+        \`\`\`json
+        
+        [  {   "officename": "Bethapudi B.O",  
+        "pincode": "522502",    
+        "officeType": "B.O",   
+        "Deliverystatus": "Delivery",   
+        "divisionname": "Guntur",   
+        "regionname": "Vijayawada",    
+        "circlename": "Andhra Pradesh",   
+        "Taluk": "Mangalagiri Ho",    
+        "Districtname": "Guntur",    
+        "statename": "ANDHRA PRADESH"  }, 
+        
+        {"officename": "Cherukupalle B.O",  
+        "pincode": "522414",    
+        "officeType": "B.O",   
+        "Deliverystatus": "Delivery",    
+        "divisionname": "Guntur",   
+        "regionname": "Vijayawada",    
+        "circlename": "Andhra Pradesh",   
+        "Taluk": "Mangalagiri Ho",    
+        "Districtname": "Guntur",    
+        "statename": "ANDHRA PRADESH"  }
+        ]
+        
+        \`\`\`
+        
+
+### Get pincode details by district
+
+Returns the details of pincodes within a specific district.
+
+- URL: **\`/pincode/district/:search\`**
+- Method: **\`GET\`**
+- Parameters:
+    - **\`search\`** (required): The name of the district to search for.
+- Response:
+    - Status code: **\`200\`**
+    - Body:
+        
+        \`\`\`json
+        
+        [{"officename": "Cherukupalle B.O",  
+        "pincode": "522414",    
+        "officeType": "B.O",   
+        "Deliverystatus": "Delivery",    
+        "divisionname": "Guntur",   
+        "regionname": "Vijayawada",    
+        "circlename": "Andhra Pradesh",   
+        "Taluk": "Mangalagiri Ho",    
+        "Districtname": "Guntur",    
+        "statename": "ANDHRA PRADESH"  }
+        ]
+
+
+`;
+
 app.get("/", (req, res) => {
-  return res.json({ success: true, message: "the server is up and runing  " });
+  return res.send(marked.parse(pincodeAPI));
 });
+
 //get the pincode details by pincode
 app.get("/api/pincode/:pincodeId", async (req, res) => {
   const pincodeId = req.params.pincodeId;
